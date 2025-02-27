4d520c83751bdfe3ef8489610f3a2af83bdd585d
jose-alejandrodiaz-by <guru.aalfa@gmail.com>
2025-02-27 15:22:30 -0600

    Refactor TasksService for task management; add postTask and getAllTasks functions; update styles in SummaryComponent for improved layout

--- aoo.front/src/services/TasksService.ts
+++ aoo.front/src/services/TasksService.ts
@@ -1,9 +1,30 @@
 import { UpdateToken,getInstance } from './UpdateToken';
 
-UpdateToken("Projects")
+UpdateToken("Tasks")
 const instance = getInstance()
 
 interface TaskData {
     // Define the structure of your project data here
     [key: string]: any;
 }
+
+const postTask = async (data: TaskData): Promise<any> => {
+    return await instance!.post('', data, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Expose-Headers': '*'
+        }
+    });
+};
+
+const getAllTasks = async (page: number): Promise<any> => {
+    return await instance!.get('', {
+        params: {
+            page: page
+        }
+    });
+};
+
+export {
+    postTask, UpdateToken, getAllTasks
+};
